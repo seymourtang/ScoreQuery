0e536967165381698f84e6903b5770b4a4e6b988
tang <tangxianmeng@live.com>
2017-05-23 23:32:43 +0800

    Project files improvement.

---
+++ ScoreQuery/LoginWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Http;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using ScoreQuery.Http;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Windows;
+namespace ScoreQuery
+{
+    /// <summary>
+    /// Interaction logic for  LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            
+        }
+        HttpHelper httpHelper = new HttpHelper();    
+        static string __EVENTTARGET = Properties.Resources.__EVENTTARGET;
+        static string __EVENTARGUMENT = Properties.Resources.__EVENTARGUMENT;
+        static string __VIEWSTATE = Properties.Resources.__VIEWSTATE;
+        static string BtnLogin = "登录";
+        static string __EVENTVALIDATION = Properties.Resources.__EVENTVALIDATION;
+        /// <summary>
+        /// 读取客户设置
+        /// </summary>
+        /// <param name="settingName"></param>
+        /// <returns></returns>
+        public static string GetSettingString(string settingName)
+        {
+            try
+            {
+                string settingString = ConfigurationManager.AppSettings[settingName].ToString();
+                return settingString;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 更新客户设置
+        /// </summary>
+        /// <param name="settingName"></param>
+        /// <param name="valueName"></param>
+        public static void UpdateSettingString(string settingName, string valueName)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (ConfigurationManager.AppSettings[settingName] != null)
+            {
+                config.AppSettings.Settings.Remove(settingName);
+            }
+            config.AppSettings.Settings.Add(settingName, valueName);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+        /// <summary>
+        /// 以异步的方式登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            btLogin.Content = "正在登录";
+            btLogin.IsEnabled = false;
+             Thread t = new Thread(DoLogin)
+            {
+                IsBackground = true
+            };
+            t.Start();
+            
+        }
+        /// <summary>
+        /// 登录的具体方法
+        /// </summary>
+        private void DoLogin()
+        {
+            try
+            {
+                string postData = string.Empty;
+                this.Invoke(new Action(() =>
+                {
+                    postData = string.Format("__EVENTTARGET={0}&__EVENTARGUMENT={1}&__VIEWSTATE={2}&TxtStudentId={3}&TxtPassword={4}&BtnLogin={5}&__EVENTVALIDATION={6}", __EVENTTARGET, __EVENTARGUMENT, __VIEWSTATE, tbStudentNo.Text, tbPassword.Password, BtnLogin, __EVENTVALIDATION);
+                }));
+                var result = httpHelper.HttpPost(ServiceURL.StudentLoginPostUrl, postData);
+                if (result.Contains("您好"))
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        MainWindow page = new MainWindow();
+                        Application.Current.MainWindow = page;
+                        this.Close();
+                        page.Show();
+                    }));
+
+                }
+                else
+                {
+                    this.Invoke(new Action(async () =>
+                    {
+                        await this.ShowMessageAsync("登录失败", "请检查账号密码!");
+                        btLogin.Content = "登录";
+                        btLogin.IsEnabled = true;
+                    }));
+                }
+            }
+            catch(Exception)
+            {
+                this.Invoke(new Action(async () =>
+                {
+                    await this.ShowMessageAsync("错误", "请检查网络连接!");
+                    btLogin.Content = "登录";
+                    btLogin.IsEnabled = true;
+                }));
+            }
+        }
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbStudentNo.Text = GetSettingString("tbStudentNo");
+            tbPassword.Password = GetSettingString("tbPassword");
+        }
+    }
+}
--- ScoreQuery/MainWindow.xaml.cs
+++ ScoreQuery/MainWindow.xaml.cs
@@ -1,128 +1,435 @@
-﻿using Http;
+﻿using HtmlAgilityPack;
+using Http;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using ScoreQuery.Http;
+using ScoreQuery.Models;
 using System;
-using System.Configuration;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
 namespace ScoreQuery
 {
     /// <summary>
-    /// Interaction logic for  MainWindow.xaml
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-        HttpHelper httpHelper = new HttpHelper();    
-        static string __EVENTTARGET = Properties.Resources.__EVENTTARGET;
-        static string __EVENTARGUMENT = Properties.Resources.__EVENTARGUMENT;
-        static string __VIEWSTATE = Properties.Resources.__VIEWSTATE;
-        static string BtnLogin = "登录";
-        static string __EVENTVALIDATION = Properties.Resources.__EVENTVALIDATION;
-        /// <summary>
-        /// 读取客户设置
-        /// </summary>
-        /// <param name="settingName"></param>
-        /// <returns></returns>
-        public static string GetSettingString(string settingName)
-        {
-            try
+            BtnAbout.Click += (s0, e0) =>
             {
-                string settingString = ConfigurationManager.AppSettings[settingName].ToString();
-                return settingString;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
+                Flyout.IsOpen = !Flyout.IsOpen;
+            };
         }
-
-        /// <summary>
-        /// 更新客户设置
-        /// </summary>
-        /// <param name="settingName"></param>
-        /// <param name="valueName"></param>
-        public static void UpdateSettingString(string settingName, string valueName)
+        HttpHelper httpHelper = new HttpHelper();
+        #region 获取成绩大表
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-
-            if (ConfigurationManager.AppSettings[settingName] != null)
-            {
-                config.AppSettings.Settings.Remove(settingName);
-            }
-            config.AppSettings.Settings.Add(settingName, valueName);
-            config.Save(ConfigurationSaveMode.Modified);
-            ConfigurationManager.RefreshSection("appSettings");
-        }
-        /// <summary>
-        /// 以异步的方式登录
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btLogin_Click(object sender, RoutedEventArgs e)
-        {
-            btLogin.Content = "正在登录";
-            btLogin.IsEnabled = false;
-             Thread t = new Thread(DoLogin)
+            BigScoreBtn.Content = "获取中...";
+            BigScoreBtn.IsEnabled = false;
+            Thread t = new Thread(GetBigScore)
             {
                 IsBackground = true
             };
             t.Start();
-            
         }
-        /// <summary>
-        /// 登录的具体方法
-        /// </summary>
-        private void DoLogin()
+
+        private void GetBigScore()
         {
             try
             {
-                string postData = string.Empty;
-                this.Invoke(new Action(() =>
+                List<BigScore> bigScore = new List<BigScore>();
+                var result = httpHelper.HttpGet(ServiceURL.BigScoreUrl);
+                string rowPath = "//*[@id=\"Table1\"]";
+                HtmlDocument doc = new HtmlDocument()
                 {
-                    postData = string.Format("__EVENTTARGET={0}&__EVENTARGUMENT={1}&__VIEWSTATE={2}&TxtStudentId={3}&TxtPassword={4}&BtnLogin={5}&__EVENTVALIDATION={6}", __EVENTTARGET, __EVENTARGUMENT, __VIEWSTATE, tbStudentNo.Text, tbPassword.Password, BtnLogin, __EVENTVALIDATION);
-                }));
-                var result = httpHelper.HttpPost(ServiceURL.StudentLoginPostUrl, postData);
-                if (result.Contains("您好"))
+                    OptionReadEncoding = false
+                };
+                doc.LoadHtml(result);
+                HtmlNode rowAount = doc.DocumentNode.SelectSingleNode(rowPath);
+                rowAount = HtmlNode.CreateNode(rowAount.OuterHtml);
+                HtmlNodeCollection name = rowAount.SelectNodes("//table");
+                var str = string.Empty;
+                string Term = string.Empty;
+                var ClassName = string.Empty;
+                var ClassNature = string.Empty;
+                var Platform = string.Empty;
+                var Grade = string.Empty;
+                var Credit = string.Empty;
+                var GradePoint = string.Empty;
+                var dt = name[0].SelectNodes("//tr//table//td");
+                BigScore big = new BigScore();
+                for (int i = 0; i < dt.Count; i++)
                 {
-                    this.Invoke(new Action(() =>
+                    if (i % 7 == 0)
                     {
-                        Window1 page = new Window1();
-                        Application.Current.MainWindow = page;
-                        this.Close();
-                        page.Show();
-                    }));
+                        Term = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 1)
+                    {
+                        ClassName = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 2)
+                    {
+                        ClassNature = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 3)
+                    {
+                        Platform = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 4)
+                    {
+                        Grade = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 5)
+                    {
+                        Credit = dt[i].InnerText.Trim();
+                    }
+                    else if (i % 7 == 6)
+                    {
+                        GradePoint = dt[i].InnerText.Trim();
+                        bigScore.Add(new BigScore
+                        {
+                            Term = Term,
+                            ClassName = ClassName,
+                            ClassNature = ClassNature,
+                            Credit = Credit,
+                            Grade = Grade,
+                            Platform = Platform,
+                            GradePoint = GradePoint
+                        });
+                    }
 
                 }
-                else
+                bigScore.RemoveAt(0);
+                this.Invoke(new Action(() =>
                 {
-                    this.Invoke(new Action(async () =>
-                    {
-                        await this.ShowMessageAsync("登录失败", "请检查账号密码!");
-                        btLogin.Content = "登录";
-                        btLogin.IsEnabled = true;
-                    }));
-                }
+                    lv.ItemsSource = bigScore;
+                    BigScoreBtn.Content = "获取";
+                    BigScoreBtn.IsEnabled = true;
+                }));
+
             }
-            catch(Exception)
+            catch (Exception ex)
             {
                 this.Invoke(new Action(async () =>
                 {
-                    await this.ShowMessageAsync("错误", "请检查网络连接!");
-                    btLogin.Content = "登录";
-                    btLogin.IsEnabled = true;
+                    await this.ShowMessageAsync("错误", ex.ToString());
+                    BigScoreBtn.Content = "获取";
+                    BigScoreBtn.IsEnabled = true;
                 }));
             }
         }
-        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+#endregion
+        #region 获取指定学期考试表
+        private void PreGetYear()
         {
-            tbStudentNo.Text = GetSettingString("tbStudentNo");
-            tbPassword.Password = GetSettingString("tbPassword");
+            var result = httpHelper.HttpGet(ServiceURL.TestDetailUrl);
+            string rowPath = "//*[@id=\"ddlYearTerm\"]";
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(result);
+            HtmlNode rowAount = doc.DocumentNode.SelectSingleNode(rowPath);
+            List<string> ls = new List<string>();
+            if (rowAount != null)
+            {
+                rowAount = HtmlNode.CreateNode(rowAount.OuterHtml);
+                HtmlNodeCollection content = rowAount.SelectNodes("//option");
+                foreach (var item in content)
+                {
+                    ls.Add(item.Attributes["value"].Value);
+                }
+            }
+            this.Invoke(new Action(() =>
+            {
+                cbYear.ItemsSource = ls;
+            }));
+        }
+        private void GetTestTabDetail()
+        {
+            try
+            {
+                var yearTerm = string.Empty;
+                this.Invoke(new Action( () =>
+                {             
+                      yearTerm = cbYear.Text;
+                }));
+                var result = httpHelper.HttpPost(ServiceURL.TestDetailUrl, Properties.Resources.TestPostData_Header + "&ddlYearTerm=" + yearTerm + "&" + Properties.Resources.TestPostData_Footer);
+                string rowPath = "//*[@id=\"Table1\"]";
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(result);
+                HtmlNode rowAount = doc.DocumentNode.SelectSingleNode(rowPath);
+                List<TestTable> ls = new List<TestTable>();
+                var ClassName = string.Empty;
+                var TeachingClass = string.Empty;
+                var Classroom = string.Empty;
+                var Time = string.Empty;
+                var TestNature = string.Empty;
+                var Teacher = string.Empty;
+                rowAount = HtmlNode.CreateNode(rowAount.OuterHtml);
+                HtmlNodeCollection content = rowAount.SelectNodes("//tr[5]//td[1]//table[1]//tr//td");
+                for (int i = 0; i < content.Count; i++)
+                {
+                    if (i % 6 == 0)
+                    {
+                        ClassName = content[i].InnerText.Trim();
+                    }
+                    else if (i % 6 == 1)
+                    {
+                        TeachingClass = content[i].InnerText.Trim();
+                    }
+                    else if (i % 6 == 2)
+                    {
+                        Classroom = content[i].InnerText.Trim();
+                    }
+                    else if (i % 6 == 3)
+                    {
+                        Time = content[i].InnerText.Trim();
+                    }
+                    else if (i % 6 == 4)
+                    {
+                        TestNature = content[i].InnerText.Trim();
+                    }
+                    else if (i % 6 == 5)
+                    {
+                        Teacher = content[i].InnerText.Trim();
+                        ls.Add(new TestTable
+                        {
+                            Classroom = Classroom,
+                            TeachingClass = TeachingClass,
+                            ClassName = ClassName,
+                            Time = Time,
+                            TestNature = TestNature,
+                            Teacher = Teacher
+                        });
+                    }
+                }
+                ls.RemoveAt(0);
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    lvTest.ItemsSource = ls;
+                    btnTest.Content = "获取";
+                    btnTest.IsEnabled = true;
+                }));
+
+            }
+            catch (Exception)
+            {
+                this.Invoke(new Action(async () =>
+                {
+                    await this.ShowMessageAsync("错误", "学期未选择或学期错误");
+                    btnTest.Content = "获取";
+                    btnTest.IsEnabled = true;
+                }));
+            }
+        }
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            btnTest.Content = "获取中...";
+            btnTest.IsEnabled = false;
+            Thread t = new Thread(GetTestTabDetail)
+            {
+                IsBackground = true
+            };
+            t.Start();
+        }
+        #endregion
+        #region 获取指定学期成绩
+        private void PreGetTermYear()
+        {
+            try
+            {
+                var result = httpHelper.HttpGet(ServiceURL.TermScoreUrl);
+                string rowPath = "//*[@id=\"ddlYearTerm\"]";
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(result);
+                HtmlNode rowAount = doc.DocumentNode.SelectSingleNode(rowPath);
+                List<string> ls = new List<string>();
+                rowAount = HtmlNode.CreateNode(rowAount.OuterHtml);
+                HtmlNodeCollection content = rowAount.SelectNodes("//option");
+                foreach (var item in content)
+                {
+                    ls.Add(item.Attributes["value"].Value);
+                }
+                ls.RemoveAt(0);
+
+                this.Invoke(new Action(() =>
+                {
+                    cbTerm.ItemsSource = ls;
+                    
+                }));
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(async () =>
+                {
+                    await this.ShowMessageAsync("错误", ex.ToString());
+                }));
+            }
+        }
+
+        private void GetTermScore()
+        {
+            try
+            {
+                var yearTerm = string.Empty;
+                this.Invoke(new Action( () =>
+                {                                                                                    
+                        yearTerm = cbTerm.Text;                    
+                }));
+                var result = httpHelper.HttpPost(ServiceURL.TermScoreUrl, Properties.Resources.TermPostData_Header + "&ddlYearTerm=" + yearTerm + Properties.Resources.TermPostData_Footer);
+                string rowPath = "//*[@id=\"Table1\"]";
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(result);
+                HtmlNode rowAount = doc.DocumentNode.SelectSingleNode(rowPath);
+                List<TermScore> ls = new List<TermScore>();
+                var ClassName = string.Empty;
+                var PlatformClass = string.Empty;
+                var NatureofReading = string.Empty;
+                var ClassNature = string.Empty;
+                var DailyScore = string.Empty;
+                var ExaminationScore = string.Empty;
+                var FinalScore = string.Empty;
+                var Credit = string.Empty;
+                var GradePoint = string.Empty;
+                rowAount = HtmlNode.CreateNode(rowAount.OuterHtml);
+                HtmlNodeCollection content = rowAount.SelectNodes("//tr//td//tr//td");
+                for (int i = 0; i < content.Count; i++)
+                {
+                    if (i % 11 == 0)
+                    {
+                        ClassName = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 1)
+                    {
+                        PlatformClass = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 4)
+                    {
+                        NatureofReading = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 5)
+                    {
+                        ClassNature = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 6)
+                    {
+                        DailyScore = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 7)
+                    {
+                        ExaminationScore = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 8)
+                    {
+                        FinalScore = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 9)
+                    {
+                        Credit = content[i].InnerText.Trim();
+                    }
+                    else if (i % 11 == 10)
+                    {
+                        GradePoint = content[i].InnerText.Trim();
+                        ls.Add(new TermScore
+                        {
+                            ClassName = ClassName,
+                            PlatformClass = PlatformClass,
+                            NatureofReading = NatureofReading,
+                            ClassNature = ClassNature,
+                            DailyScore = DailyScore,
+                            ExaminationScore = ExaminationScore,
+                            FinalScore = FinalScore,
+                            Credit = Credit,
+                            GradePoint = GradePoint
+                        });
+                    }
+                }
+                ls.RemoveAt(0);
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    lvTerm.ItemsSource = ls;
+                    btnTerm.Content = "获取";
+                    btnTerm.IsEnabled = true;
+                }));
+            }
+            catch (Exception)
+            {
+                this.Invoke(new Action(async () =>
+                {
+                    await this.ShowMessageAsync("错误", "学期未选择");
+                    btnTerm.Content = "获取";
+                    btnTerm.IsEnabled = true;
+                }));
+            }
+        }
+        private void btnTerm_Click(object sender, RoutedEventArgs e)
+        {
+            btnTerm.Content = "获取中...";
+            btnTerm.IsEnabled = false;
+            Thread t = new Thread(GetTermScore)
+            {
+                IsBackground = true
+            };
+            t.Start();
+        }
+        #endregion
+
+        #region 选项卡的预获取信息
+        private void tbControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tbControl.SelectedIndex == 1)
+            {
+                Thread t = new Thread(PreGetTermYear)
+                {
+                    IsBackground = true
+                };
+                t.Start();
+            }
+            else if (tbControl.SelectedIndex == 2)
+            {
+                Thread t = new Thread(PreGetYear)
+                {
+                    IsBackground = true
+                };
+                t.Start();
+            }
+        }
+        #endregion
+
+        private void EmailButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mailto://tangxianmeng@live.com");
+        }
+
+        private void WeiboButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://weibo.com/tangxianmeng");
+        }
+
+
+        private void GitHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/seymourtang");
+        }
+
+        private void TwitterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://twitter.com/Tangxianmeng");
         }
     }
 }
